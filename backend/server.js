4118f98604b1f5a0104ed34278527df69233baf5
Amirul Ikhwan <amirikhwanfaisal@gmail.com>
2025-11-01 12:08:47 +0800

    change route

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
 app.use(cors({
-    origin : 'http://localhost:5173' || process.env.FRONTEND_URL,
+    origin : process.env.FRONTEND_URL  || 'http://localhost:5173',
     methods: ['GET' , 'POST' , 'PUT' , 'DELETE'],
     allowedHeaders: ['Content-Type'],
     credentials: true
